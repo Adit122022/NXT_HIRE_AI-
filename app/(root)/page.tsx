27af7d8b0595a9e65e60549545bc03ce8a4f58c4
Adit122022 <adity202212@gmail.com>
2025-07-02 22:43:56 +0530

    feat(layout): enhance layout with navigation and logo integration feat(page): improve home page with interview preparation section and dynamic interview cards fix(constants): restore dummy interview data for testing chore(package): add dayjs dependency for date handling feat(InterviewCard): create InterviewCard component for displaying interview details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,43 @@
+import InterviewCard from '@/components/InterviewCard'
+import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-     Home Page 
+  <>
+  <section className="card-cta">
+    <div className="flex flex-col max-w-lg space-y-2">
+      <h2>Get Interview Ready With AI-Powered Practice & Feedback</h2>
+      <p className="text-lg">
+        Practice on real Interview  questions and Get instant feedback
+      </p>
+      <Button asChild className='btn-primary max-sm:w-full'>
+      <Link href="/interview">Start an Interview</Link>  
+      </Button>
     </div>
+    <Image src="/robot.png" alt='Robo-Dude' width={400} height={400} className='max-sm:hidden'/>
+  </section>
+  {/* INterview Section */}
+  <section className="flex  flex-col gap-6 mt-8">
+    <h2>Your Interviews</h2>
+    <div className="interiew_section">
+      {dummyInterviews.map((interview)=>(
+        <InterviewCard {...interview}/>
+      ))}
+      <p>You haven't  taken any interviews yet</p>
+    </div>
+  </section>
+  <section className="flex  flex-col gap-6 mt-8">
+    <h2>Take an Interviews</h2>
+    <div className="interiew_section">
+      <p>There are no Interviews available</p>
+    </div>
+  </section>
+  
+  </>
   )
 }
 
