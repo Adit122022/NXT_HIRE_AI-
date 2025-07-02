c1eb01d5b9c18c04f2ce76ecf877a3b3d585b1e5
Adit122022 <adity202212@gmail.com>
2025-07-02 12:51:24 +0530

    feat(auth): refactor AuthForm component with form handling and validation

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,10 @@
-import AuthForm from '@/components/AuthForm'
+import AuthForm from '../../../components/AuthForm'
 import React from 'react'
 
 const page = () => {
-  return <AuthForm type="sign-in"/>
+  return (
+  <AuthForm type="sign-in"/>
+)
 }
 
 export default page
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,76 @@
-import React from 'react'
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import React from 'react';
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import Image from "next/image";
+import Link from "next/link";
+import { toast } from "sonner";
+import FormField from "./FormField";
 
-const AuthForm = () => {
+
+
+const authFromSchema =(type :FormType)=>{
+     return  z.object({
+  name:type === "sign-up" ? z.string().min(3): z.string(),
+  email:z.string().email(),
+  password:z.string().min(3),
+})
+
+}
+
+const AuthForm = ({type} :{type :FormType}) => {
+    const formSchema = authFromSchema(type)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+    name:'',
+    email:'',
+    password:''
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+      try {
+        if(type=== 'sign-up') console.log('Sign Up' , values);
+        else console.log('Sign IN' , values);
+          
+    } catch (error) {
+          console.log(error)
+toast.error(`There was an error: ${error}`)        
+      }
+
+  }
+
+  const isSignIN = type=== "sign-in"
   return (
-    <div>AuthForm</div>
+    <div className="card-border lg:min-w-[566px]">
+        <div className="flex flex-col gap-6 card py-14 px-10">
+            <div className="flex flex-row gap-2 justify-center">
+                <Image src="/logo.svg" alt="logo" height={32} width={38}/>
+                <h2>AI_HR</h2>
+            </div>
+                <h2>Practice Job Interview With Ai</h2>
+        
+         <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full mt-4 form">
+      {!isSignIN && <FormField control={form.control} name={"name"} label={"Name"} placeholder="Your Name"/>}
+      <FormField control={form.control} name={"email"} label={"Email"} placeholder="Your Email"/>
+      <FormField control={form.control} name={"password"} label={"Password"} placeholder="Your Password"/>
+        <Button className="btn" type="submit">{isSignIN ? "Sign IN" : "Create an Account"}</Button>
+      </form>
+    </Form>
+    <p className="text-center">
+
+{isSignIN ?" no account yet ?" :" have an account already "}
+<Link href={!isSignIN ? '/sign-in' : "/sign-up"} className="font-bold text-user-primary ml-1">{!isSignIN ? "Sign In" : "Sign up"} 
+</Link>
+    </p>
+    </div>
+    </div>
   )
 }
 
