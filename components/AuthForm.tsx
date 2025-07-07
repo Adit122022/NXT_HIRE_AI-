f08a2194872f8d34dc0f7a09ec46966e216becd3
Adit122022 <adity202212@gmail.com>
2025-07-07 10:29:17 +0530

    feat(AuthForm): implement user signup functionality with Firebase integration
    1:39:35

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,8 +10,9 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
-
-
+import { auth } from "@/firebase/client";
+import { createUserWithEmailAndPassword} from "firebase/auth";
+import { signup } from "@/lib/actions/auth.actions";
 
 const authFromSchema =(type :FormType)=>{
      return  z.object({
@@ -35,9 +36,20 @@ const  AuthForm = ({type} :{type :FormType}) => {
     },
   })
  
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  aysnc function onSubmit(values: z.infer<typeof formSchema>) {
       try {
-        if(type=== 'sign-up'){ console.log('Sign Up' , values);
+        if(type=== 'sign-up'){
+          const {name , email , password} = values;
+          const userCredentials = await createUserWithEmailAndPassword(auth ,email ,password);
+           const result = await signup({
+            uid :userCredentials.user.uid,
+            name:name!,
+            email, password 
+           })
+           if(!result?.success){
+            toast.error(result?.message)
+            return;
+           }
             toast.success('Account Created Succsfully.Please Sign In')
              router.push('/sign-in')
         }
---
+++ lib/actions/auth.actions.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { db } from "@/firebase/admin";
+
+export async function signup(params :SignUpParams){
+const  {uid , email , name} = params;
+ try {
+     const userRecord = await  db.collection("users").doc(uid).get();
+     if(userRecord.exists){
+        return { success:false , message:'User already exists .Please sign in instead.'}
+     }
+     await db.collection('users').doc(uid).set({ name, email })
+ } catch (e :any) {
+     console.log("Error creating a user" , e);
+     if(e.code === 'auth/email-already-exists'){
+        return { success: false , message :'This email is already in use.' }
+     }
+     return {success : false , message: ' Failed to create an account.'}
+ }
+}
