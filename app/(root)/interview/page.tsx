68319bbfa7b7f5d5b2cc42ca9929f4a44fdcad53
Adit122022 <adity202212@gmail.com>
2025-07-24 01:03:39 +0530

    feat(interview): add interview page with Agent component integration

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Agent userName ="You" userId = "user1" type="generate" />
+      interview Page
+    </div>
+  )
+}
+
+export default page
