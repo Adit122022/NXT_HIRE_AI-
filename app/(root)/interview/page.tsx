729954978dc50d9875b8e3517cdef34f95eceef0
Adit122022 <181944095+Adit122022@users.noreply.github.com>
2025-08-08 15:29:08 +0530

    implemented interview genaration page ui

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <>
+      <h3>Interview Generation</h3>
       <Agent userName ="You" userId = "user1" type="generate" />
-      interview Page
-    </div>
+     
+    </>
   )
 }
 
